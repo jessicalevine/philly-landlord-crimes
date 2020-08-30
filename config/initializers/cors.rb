de5a77c5c6b327915fb8e5df148c5fb7c53b635c
Jessica Levine <jessica.s.levine@gmail.com>
2020-08-30 18:31:19 -0400

    Enable CORS: TODO / WARNING: Improve before deploy
    Opens up cross-origin requests from anywhere to ease early development,
    based on this guide:
    
    https://johncorderox.com/2019/scaffold-a-new-rails-5-api/
    
    We should restrict requests to just the front-end domain before our
    first production deploy.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+# TODO Restrict to only front-end domain before deploying
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'example.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
