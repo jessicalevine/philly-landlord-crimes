cc2140adee10aff371e1f8c71eb1c0ac03914ee2
asherr <aaron.sherrin@gmail.com>
2020-09-10 17:00:32 -0400

    Add seed task to load sample data into RealEstateTaxDelinquencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+deliquency_path = File.expand_path("./data/real_estate_tax_delinquencies.sample.csv")
+
+CSV.foreach(deliquency_path, headers: true) do |row|
+   RealEstateTaxDelinquencies.create(row.to_hash.except("return_mail"))
+end
